74729bd5a06be8564e88025e04397bbf750f7038
GabrielJonatas <gabrieljonatas2000@hotmail.com>
2024-12-16 14:30:14 -0300

    feat: criacao do model, repository e controller do produto

--- src/main/java/com/generation/crudFarmacia/model/Categoria.java
+++ src/main/java/com/generation/crudFarmacia/model/Categoria.java
@@ -1,13 +1,19 @@
 package com.generation.crudFarmacia.model;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -33,6 +39,10 @@ public class Categoria {
 
 	@UpdateTimestamp
 	private LocalDateTime datacriacao;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
 
 	public Long getId() {
 		return id;
@@ -72,6 +82,14 @@ public class Categoria {
 
 	public void setDatacriacao(LocalDateTime datacriacao) {
 		this.datacriacao = datacriacao;
+	}
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
 	}
 	
 }
---
+++ src/main/java/com/generation/crudFarmacia/model/Produto.java
@@ -0,0 +1,103 @@
+package com.generation.crudFarmacia.model;
+
+import java.math.BigDecimal;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo nome é obrigatório!")
+	@Size(max = 150, message = "O atributo nome deve conter no máximo 150 caracteres")
+	private String nome;
+	
+	@Size(max = 255, message = "O atributo descricao deve conter no máximo 255 caracteres")
+	private String descricao;
+
+	@NotNull(message = "O atributo preco é obrigatório")
+	@Digits(integer = 7, fraction = 2, message = "O preco deve conter 7 digitos inteiros com 2 casas decimais")
+	private BigDecimal preco;
+	
+	@NotNull(message = "O atributo desconto é obrigatório")
+	private Integer desconto;
+	
+	@NotNull(message = "O atributo quantidadeEstoque é obrigatório")
+	private Integer quantidadeEstoque;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public Integer getDesconto() {
+		return desconto;
+	}
+
+	public void setDesconto(Integer desconto) {
+		this.desconto = desconto;
+	}
+
+	public Integer getQuantidadeEstoque() {
+		return quantidadeEstoque;
+	}
+
+	public void setQuantidadeEstoque(Integer quantidadeEstoque) {
+		this.quantidadeEstoque = quantidadeEstoque;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+}
